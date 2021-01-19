975104b525188c42cd51e1ba00601f6ba7693051
kevin-ba <kevin.bauer2@gmx.de>
2021-01-19 03:26:24 +0100

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about" pa-6>
+    <h1>About Reversi</h1>
+    <p>
+        Reveris ist ein inzwischen klassisches 2 Personen Brettspiel, das unter verschiedenen teilweise geschützen Namen
+        bekannt ist.
+        In der realen Welt spielt man mit Steinen, die zwischen auf einer Seite weiß und auf der anderen Schwarz sind.
+        Da im Spielverlauf dauernd Steine durch Umdrehen die Farbe wechseln, ist Reveris sehr dynamisches Spiel.
+    </p>
+
+    <p>
+        Spielablauf
+        Das Spielbrett hat 8x8 Felder, üblicherweise verwendet man Schachkoordinaten (Spalten mit Buchstaben a-h, Zeilen mit Ziffern 1-8).
+        Die Startstellung ist leer, bis auf die vier Felder in der Mitte.
+        Die Spieler kommen abwechselnd an die Reihe.
+        Schwarz das Spiel beginnt mit dem ersten Zug.
+    </p>
+     <p>
+        Schwarz beginnt. <br>
+        <br> Bei seinem Zug legt ein Spieler einen Stein seiner eigenen Farbe auf ein leeres Feld.
+        Durch den Zug müssen ein oder mehrere gegnerische Steine geradlinig (auch diagonal) ohne Lücke zwischen den gerade gespielten eigenen Stein und bereits
+        liegende eigene Steine eingeklammert werden. Die eingeklammerten gegnerischen Steine wechseln dann die Farbe, werden also eigene Steine. <br>
+    </p>
   </div>
 </template>
